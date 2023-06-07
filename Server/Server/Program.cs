83ed112f09df7337364a7e11c26d200cf8f4b952
hun1001 <l01022899266@gmail.com>
2023-06-07 09:20:03 +0900

    시작 코드 세팅

---
+++ Server/Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Google.Protobuf.Protocol;
+using Google.Protobuf.WellKnownTypes;
+using Server.Data;
+using Server.Game;
+using ServerCore;
+
+namespace Server
+{
+	class Program
+	{
+		static Listener _listener = new Listener();
+
+		static void FlushRoom()
+		{
+			JobTimer.Instance.Push(FlushRoom, 250);
+		}
+
+		static void Main(string[] args)
+		{
+			ConfigManager.LoadConfig();
+			DataManager.LoadData();
+
+			RoomManager.Instance.Add(1);
+
+			// DNS (Domain Name System)
+			string host = Dns.GetHostName();
+			IPHostEntry ipHost = Dns.GetHostEntry(host);
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+			_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
+			Console.WriteLine("Listening...");
+
+			//FlushRoom();
+			//JobTimer.Instance.Push(FlushRoom);
+
+			// TODO
+			while (true)
+			{
+				//JobTimer.Instance.Flush();
+				RoomManager.Instance.Find(1).Update();
+				//Thread.Sleep(100);
+			}
+		}
+	}
+}
